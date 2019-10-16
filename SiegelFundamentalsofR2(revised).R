ff17acd6aaa63d8361bbae8af18137c8f5cfd0de
Vinnie Siegel <jvs0010@mix.wvu.edu>
2019-10-16 09:44:47 -0400

    Initial Commit

---
+++ SiegelFundamentalsofR2(revised).R
@@ -0,0 +1,17 @@
+h <- c( seq( 1, 50, 1)) #Question 1
+length(h) 
+h[10 : 15] #Question 2
+mean(h) #Question 3
+sum(h > 5) #Question 4
+i <- matrix( c(1:150), 10, 15) #Question 5
+matrixmean <- function(x){
+  average <- ( sum(x) / length(x) )
+  return(average)
+}
+matrixmean(i) #Question 6
+avgrow <- c(1:10)
+y <- 1
+for(y in 1:length( i[ ,y] )){
+  avgrow[y] <- matrixmean(i[y,])
+}
+avgrow #Question 7
